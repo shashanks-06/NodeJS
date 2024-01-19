0c16bf3d8bf3ba629f56f40748aec98ef523f618
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-01-19 16:56:17 +0530

    Add files via upload

---
+++ 1.Fundamentals,Modules,Streams,Events/index.js
@@ -0,0 +1,8 @@
+import * as calculator from "./calculator.mjs";
+
+console.log(calculator.add(6, 3));
+console.log(calculator.subtract(6, 3));
+console.log(calculator.multiplication(6, 3));
+console.log(calculator.division(6, 3));
+console.log(calculator.module(6, 4));
+console.log(calculator.factorial(4));
