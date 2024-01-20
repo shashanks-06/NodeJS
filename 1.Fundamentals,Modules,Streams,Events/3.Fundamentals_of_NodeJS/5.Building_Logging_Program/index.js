4761aa3174e684b1a948b0c8e7dd6e1527fa99eb
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-01-20 13:12:25 +0530

    Add files via upload

--- 1.Fundamentals,Modules,Streams,Events/3.Fundamentals_of_NodeJS/5.Building_Logging_Program/index.js
+++ 1.Fundamentals,Modules,Streams,Events/3.Fundamentals_of_NodeJS/5.Building_Logging_Program/index.js
@@ -1,1 +1,6 @@
+import { writeLogLine, readLogFile, appendLogLine } from "./logger.js";
 
+writeLogLine("log.txt", "test line\n");
+readLogFile("log.txt");
+appendLogLine("log.txt", "second test line\n");
+readLogFile("log.txt");
