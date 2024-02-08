22aa5abb210dd4e66d0bd304d6e7ece9371fbc1d
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-02-08 12:43:43 +0530

    Update httpServer.js

--- 1.Fundamentals,Modules,Streams,Events/8.Handling_HTTP_HTTPS/6.Project--Creating_HTTP_Server/httpServer.js
+++ 1.Fundamentals,Modules,Streams,Events/8.Handling_HTTP_HTTPS/6.Project--Creating_HTTP_Server/httpServer.js
@@ -14,13 +14,47 @@ http
 
       req.on("end", () => {
         body = JSON.parse(body);
-        users[body.username] = body.password;
+
+        const index = users.indexOf(users.username);
+        if (index == -1) {
+          users[body.username] = body.password;
+        }
         console.log(users);
       });
     } else if (req.method === "GET") {
       const parse = url.parse(req.url);
       //   console.log(parse.query);
       console.log(users[parse.query]);
+    }
+      // Part 2 -- PUT and DELETE
+      
+    else if (req.method === "PUT") {
+      let body = "";
+
+      req.on("data", (data) => {
+        body += data;
+      });
+
+      req.on("end", () => {
+        body = JSON.parse(body);
+        users[body.username] = body.password;
+        console.log(users);
+      });
+    } else if (req.method === "DELETE") {
+      let body = "";
+
+      req.on("data", (data) => {
+        body += data;
+      });
+
+      req.on("end", () => {
+        body = JSON.parse(body);
+        const index = users.indexOf(users.username);
+        if (index > -1) {
+          users.splice(index, 1);
+        }
+        console.log(users);
+      });
     }
     res.end();
   })
