2618f902039c376758d3ec23fb4e1b1bb4579417
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-02-08 12:28:59 +0530

    Create httpServer.js

---
+++ 1.Fundamentals,Modules,Streams,Events/8.Handling_HTTP_HTTPS/6.Project--Creating_HTTP_Server/httpServer.js
@@ -0,0 +1,27 @@
+import http from "http";
+import url from "url";
+
+http
+  .createServer(function (req, res) {
+    // Part 1 -- GET and POST
+    
+    if (req.method === "GET") {
+      res.write("GET request received");
+    } else if (req.method === "POST") {
+      let body = "";
+
+      req.on("data", (data) => {
+        body += data;
+      });
+
+      req.on("end", () => {
+        body = JSON.parse(body);
+        console.log(body);
+        console.log(body.username);
+      });
+    }
+    res.end();
+  })
+  .listen(80);
+
+//send post request on postman.
